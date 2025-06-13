e1ef5fc5addf53615674dc9dddebd13321ff2e46
Cass0r <carjose2000@gmail.com>
2025-06-13 16:56:37 -0400

    adding jakarta.persistence and hibernate.cfg.xml

--- src/main/java/com/example/Items.java
+++ src/main/java/com/example/Items.java
@@ -1,4 +1,9 @@
 package com.example;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 public class Items {
     //These are the Variables used for the items information
